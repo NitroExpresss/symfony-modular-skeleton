aecd88719554c91b82c82b2cf9cc1667d7bd6d5d
Robert <robertgibizov@gmail.com>
2025-05-13 14:12:00 +0300

    added yadro as global service

--- config/services.php
+++ config/services.php
@@ -5,5 +5,9 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $di): void {
+    Introvert\Configuration::getDefaultConfiguration()->setApiKey('key', '%env(INTROVERT_YADRO_API_KEY)%');
+    $di->services()
+        ->set('introvert.api_client', \Introvert\ApiClient::class)
+        ->public();
     $di->import('../src/**/di.php');
 };
